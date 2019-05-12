899ceaf78daee87f2d64ec23a821e52b07b93205
Ashwin Athrey Yellapantula <ashwinathrey93@gmail.com>
2019-05-12 17:54:11 -0400

    Update SlackNotification.groovy

--- vars/SlackNotification.groovy
+++ vars/SlackNotification.groovy
@@ -1,26 +1,4 @@
-Skip to content
- 
-Search or jump to…
 
-Pull requests
-Issues
-Marketplace
-Explore
- 
-@yellapantula 
- The password you provided is weak and can be easily guessed. To increase your security, please change your password as soon as possible.
-
-Read our documentation on safer password practices.
-
-1
-1 17 lvthillo/shared-library
- Code  Issues 0  Pull requests 0  Projects 0  Wiki  Insights
-shared-library/vars/slackNotifier.groovy
-@lvthillo lvthillo Update slackNotifier.groovy
-ec1e675 on Apr 11, 2018
-17 lines (15 sloc)  663 Bytes
-    
-#!/usr/bin/env groovy
 
 def call(String buildResult) {
   if ( buildResult == "SUCCESS" ) {
@@ -36,15 +14,3 @@ def call(String buildResult) {
     slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} its resulat was unclear"	
   }
 }
-© 2019 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Help
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
